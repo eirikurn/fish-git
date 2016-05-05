301f1802bcde3382354c0dc932dafe347521c1eb
Eirikur Nilsson <eirikur@nilsson.is>
2016-05-05 23:36:43 +0000

    Initial commit

---
+++ init.fish
@@ -0,0 +1,10 @@
+function init -a path --on-event init_eirikurn-git
+  abbr g 'git status -sb'
+  abbr gc 'git commit'
+  abbr gca 'git commit -a'
+  abbr gl 'git pull'
+  abbr gp 'git push'
+  abbr gd 'git diff'
+  abbr ga 'git add'
+  abbr gaa 'git add --all'
+end
