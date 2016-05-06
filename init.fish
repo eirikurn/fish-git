581a7aa1185d2ea6332d4b6af466e28fb7c6ceb3
Eirikur Nilsson <eirikur@nilsson.is>
2016-05-06 00:29:46 +0000

    Add more commands

--- init.fish
+++ init.fish
@@ -7,4 +7,7 @@ function init -a path --on-event init_eirikurn-git
   abbr gd 'git diff'
   abbr ga 'git add'
   abbr gaa 'git add --all'
+  abbr gco 'git checkout'
+  abbr gb 'git branch'
+  abbr glog "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 end
